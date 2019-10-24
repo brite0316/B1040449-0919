cc17fce71a7b477d99b44c9aa9f8351c74d2438f
brite0316 <55218580+brite0316@users.noreply.github.com>
2019-10-24 10:01:42 +0800

    B1040449-1024

---
+++ Motioncapture/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Animator myAnimator;
+	// Use this for initialization
+	void Start () {
+        myAnimator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnimator.SetTrigger("跳舞觸發氣");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            myAnimator.SetBool("跑步開關", true);
+        }
+        else
+        {
+            myAnimator.SetBool("跑步開關", false);
+        }
+    }
+
+}
