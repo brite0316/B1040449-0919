85d8acfb9b5f400ae02ee5df61090fa69231b743
brite0316 <55218580+brite0316@users.noreply.github.com>
2019-10-31 10:24:38 +0800

    角色設定類別方法1031

---
+++ Motioncapture/Assets/Scripts/lady.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public class lady : MonoBehaviour
+{
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void walk()
+    {
+
+    }
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    private void Hurt()
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+}
