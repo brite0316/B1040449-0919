770798cc7787160f5ecd354217a6d34f796bb98e
ben.yeh0316@gmail.com <Giorge1234>
2019-11-21 10:30:24 +0800

    1121

--- Motioncapture/Assets/Scripts/lady.cs
+++ Motioncapture/Assets/Scripts/lady.cs
@@ -1,46 +1,70 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
-public class lady : MonoBehaviour
+public class Lady : MonoBehaviour
 {
-    private Animator ani;       //動畫元件
-    private Rigidbody rig;      //剛體元件
+    private Animator ani;               // 動畫元件
+    private Rigidbody rig;              // 剛體元件
 
-    [Header("速度"), Range(0f, 80f)]
-     public float speed = 1.5f;
+    [Header("走路速度"), Range(0f, 80f)]
+    public float speed = 1.5f;
+    [Header("旋轉速度"), Range(1f, 100f)]
+    public float turn = 1.5f;
 
-    [Header("動畫控制器:參數名稱")]
-    public string parRun = "走路開關";
+    [Header("動畫控制器：參數名稱")]
+    public string parRun = "跑步開關";
     public string parAtk = "攻擊觸發";
     public string parDam = "受傷觸發";
-    public string parjump = "跳躍觸發";
+    public string parJump = "跳躍觸發";
     public string parDead = "死亡開關";
-
 
     private void Start()
     {
-        ani = GetComponent<Animator>();
+        ani = GetComponent<Animator>();     // 動畫元件欄位 =  取得元件<泛型>();
         rig = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        Turn();
+        Attack();
+    }
+
+    // FixedUpade 1 格執行 0.002 秒 (有使用物理寫在這裡)
     private void FixedUpdate()
     {
         Walk();
-        Attack();
         Jump();
     }
-    //定義方法
-    //修飾詞 傳回類型 方法名稱 (參數) (敘述)
-    //woid 無回傳
+
+    // 定義方法
+    // 修飾詞 傳回類型 方法名稱 (參數) { 敘述 }
+    // void 無回傳
 
     /// <summary>
-    /// 走路
+    /// 前後左右走路
     /// </summary>
     private void Walk()
     {
-        ani.SetBool(parRun,Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0);
-        //rig.AddForce(0 , 0, Input.GetAxisRaw("Vertical") * speed)://以世界座標移動
-        rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed);
+        // 動畫：跑步 - 按下前後時 true
+        ani.SetBool(parRun, Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0);
+        //rig.AddForce(0, 0, Input.GetAxisRaw("Vertical") * speed);                 // 以世界座標移動
+        //rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed);   // 以區域座標移動
+
+        // 前方 transform.forward (0, 0, 1)
+        // 右方 transform.right   (1, 0, 0)
+        // 上方 transform.up      (0, 1, 0)
+        rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed + transform.right * Input.GetAxisRaw("Horizontal") * speed);
+    }
+
+    /// <summary>
+    /// 左右旋轉
+    /// </summary>
+    private void Turn()
+    {
+        float x = Input.GetAxis("Mouse X");   // 滑鼠左右，左 -1、右 1
+        //print("玩家滑鼠 X：" + x);
+        // Time.deltaTime 一幀的時間
+        transform.Rotate(0, x * turn * Time.deltaTime, 0);
     }
 
     /// <summary>
@@ -48,7 +72,7 @@ public class lady : MonoBehaviour
     /// </summary>
     private void Attack()
     {
-         if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
             ani.SetTrigger(parAtk);
     }
 
@@ -58,7 +82,7 @@ public class lady : MonoBehaviour
     private void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            ani.SetTrigger(parjump);
+            ani.SetTrigger(parJump);
     }
 
     /// <summary>
